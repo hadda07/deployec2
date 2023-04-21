176803d57841321401035b626b3c5982bc2aa2ff
hadda07 <73991026+hadda07@users.noreply.github.com>
2023-04-21 23:21:14 +0530

    Update startec2.py

--- startec2.py
+++ startec2.py
@@ -2,7 +2,7 @@
 import boto3
 AWS_REGION = "eu-west-1"
 EC2_RESOURCE = boto3.resource('ec2', region_name=AWS_REGION)
-INSTANCE_ID = 'i-0db193fb2af4a35f5'
+INSTANCE_ID = 'i-0c21e3ec11c46e249'
 instance = EC2_RESOURCE.Instance(INSTANCE_ID)
 instance.start()
 print(f'Starting EC2 instance: {instance.id}')
